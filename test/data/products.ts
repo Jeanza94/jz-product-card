3ea4f3467df7e551a75d1febe89027c8ea91ac16
Jean Zapata <jeanza94@hotmail.com>
2022-09-06 07:21:52 -0500

    version 0.0.1 lista

---
+++ test/data/products.ts
@@ -0,0 +1,12 @@
+
+export const product1 = {
+    id: '1',
+    title: 'coffee-mug - card-sin imagen',
+    // img: './coffee-mug.png'
+}
+
+export const product2 = {
+    id: '1',
+    title: 'coffee-mug - card - con imagen',
+    img: './coffee-mug.png'
+}
